26010ce92896b63223108f9d01c60b0c03ca95a0
nwcubeok <nwcubeok@gmail.com>
2024-12-05 20:06:47 +0100

    Menu visible mais erreur separator

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,17 @@
+import React from "react";
+
+
 import Image from "next/image";
 import TypingAnimation from "@/components/ui/typing-animation";
+import Navbar from "@/components/navbar";
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        
+      <Navbar/>
         <Image
           className="dark:invert"
           src="/next.svg"
@@ -14,9 +21,12 @@ export default function Home() {
           priority
         />
         <TypingAnimation
-      className="text-4xl font-bold text-black dark:text-white"
-      text="Typing Animation"
-    />
+          className="text-4xl font-bold text-black dark:text-white"
+          text="Typing Animation"
+        />
+        <div className="relative">
+          
+        </div>
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
             Get started by editing{" "}
@@ -104,3 +114,4 @@ export default function Home() {
     </div>
   );
 }
+
---
+++ src/components/navbar.tsx
@@ -0,0 +1,110 @@
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import { HomeIcon, GithubIcon } from "lucide-react";
+
+import { cn } from "@/lib/utils";
+import { buttonVariants } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip";
+import { ModeToggle } from "@/components/mode-toggle";
+import { Dock, DockIcon } from "@/components/ui/dock";
+
+export type IconProps = React.HTMLAttributes<SVGElement>;
+
+const JojoLogo = () => {
+  return (
+      <Image
+        className="dark:invert"
+        src="/jojo_logo.svg"
+        alt="Next.js logo"
+        width={18}
+        height={18}
+        priority
+      />
+  )
+}
+const Icons = {
+  home: HomeIcon,
+  github: GithubIcon,
+  jojo: JojoLogo,
+};
+
+const NAVBAR_DATA = [
+  { href: "#", icon: Icons.home, label: "Home" },
+  { href: "#", icon: Icons.jojo, label: "Jojo" },
+];
+
+const CONTACT_DATA = {
+  social: {
+    GitHub: {
+      name: "GitHub",
+      url: "#",
+      icon: Icons.github, 
+    },
+  },
+};
+
+export function Navbar() {
+  return (
+    <TooltipProvider>
+      <Dock direction="middle">
+        {NAVBAR_DATA.map((item) => (
+          <DockIcon key={item.label}>
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <Link
+                  href={item.href}
+                  aria-label={item.label}
+                  className={cn(
+                    buttonVariants({ variant: "ghost", size: "icon" }),
+                    "size-12 rounded-full"
+                  )}
+                >
+                  <item.icon className="size-4" />
+                </Link>
+              </TooltipTrigger>
+              <TooltipContent>
+                <p>{item.label}</p>
+              </TooltipContent>
+            </Tooltip>
+          </DockIcon>
+        ))}
+        <Separator orientation="vertical" className="h-full" />
+        {Object.entries(CONTACT_DATA.social).map(([name, social]) => (
+          <DockIcon key={name}>
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <Link
+                  href={social.url}
+                  aria-label={social.name}
+                  className={cn(
+                    buttonVariants({ variant: "ghost", size: "icon" }),
+                    "size-12 rounded-full"
+                  )}
+                >
+                  <social.icon className="size-4" />
+                </Link>
+              </TooltipTrigger>
+              <TooltipContent>
+                <p>{name}</p>
+              </TooltipContent>
+            </Tooltip>
+          </DockIcon>
+        ))}
+        <DockIcon>
+          <Tooltip>
+            <TooltipTrigger asChild>
+              <ModeToggle />
+            </TooltipTrigger>
+            <TooltipContent>
+              <p>Theme</p>
+            </TooltipContent>
+          </Tooltip>
+        </DockIcon>
+      </Dock>
+    </TooltipProvider>
+  );
+}
+
+export default Navbar;
