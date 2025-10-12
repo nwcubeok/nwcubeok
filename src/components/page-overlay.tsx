b3a12ae8e58f45862ea73b0d49fc2732ab475b84
nwcubeok <nwcubeok@gmail.com>
2025-10-12 02:23:24 +0200

    Fix removeChildNode error

--- src/components/page-overlay.tsx
+++ src/components/page-overlay.tsx
@@ -1,10 +1,9 @@
 
-import React, { useState, useEffect } from "react"; 
+import React, { useState, useEffect, useRef } from "react"; 
 import {InteractiveGridPattern} from "@/components/ui/interactive-grid-pattern";
 import { useTheme } from "next-themes";
 
 const PageOverlay = () => {
-    const { resolvedTheme } = useTheme();
     const [mounted, setMounted] = useState(false);
 
     // Ne pas rendre le composant tant que le thème n'est pas chargé
@@ -15,12 +14,10 @@ const PageOverlay = () => {
     if (!mounted) return null;
 
     return (
-
-        <div className="fixed w-screen h-screen">
+        <div className="fixed inset-0 pointer-events-none z-0">
             <InteractiveGridPattern
                 squares={[50, 50]}
-                className={"h-full w-full z-0"}
-                //fixedSquares={{"597" : color, "648" : color, "697" : color}}
+                className="h-full w-full"
             />
         </div>
     );
